77530e94fd67173b4381ff002046f7ad5de3f5ab
Jeff Colvin <jcolvin2012@my.fit.edu>
2020-07-18 20:58:36 -0400

    updated wave height calculations from timestack

--- src/R/WaveHeight_TS.R
+++ src/R/WaveHeight_TS.R
@@ -3,9 +3,13 @@
 # pixel location of wave crest and trough #
 # from distortion-corrected timestack.    #
 #                                         #
-# Jeff Colvin                             #
-# August 2017                             #
+#      Jeff Colvin, August 2017           #
+#         Updated July 2020               #
 ###########################################
+
+# An example wavedata.txt file is included in the sample data directory
+# This is a text file with the pixel locations of wave crests and troughs
+# from a timestack image. Research is underway to automate this process.
 
 filename <- 'wavedata.txt'
 wavedata <- read.csv(file=filename, header=TRUE, sep=",")
@@ -13,10 +17,10 @@ wavedata <- read.csv(file=filename, header=TRUE, sep=",")
 crest <- wavedata$crest     # y-pixel locations of crest
 trough <- wavedata$trough   # y-pixel locations of trough
 
-cam.height <- 100           # camera height in cm above SWL
+cam.height <- 98            # camera height in cm above SWL
 op.axis <- 534              # y-pixel of optical axis (from cam calibration file)
 
-k <- 1/3                    # amount of wave below SWL
+k <- 1/3                    # amount of wave below SWL (see article)
 
 swl <- trough - k * (trough - crest)
 
